7adf1dace6c0ab7d3c95e9293516afbbca7c029c
Kas-ES <55737559+Kas-ES@users.noreply.github.com>
2023-12-28 08:16:23 +0100

    revert merge

--- server.R
+++ server.R
@@ -1,283 +1,32 @@
 if(!requireNamespace("shiny",quietly=TRUE))install.packages("shiny")
 if(!requireNamespace("dplyr",quietly=TRUE))install.packages("dplyr")
 if(!requireNamespace("ggplot2",quietly=TRUE))install.packages("ggplot2")
-if(!requireNamespace("gganimate",quietly=TRUE))install.packages("gganimate")
-if(!requireNamespace("gifski",quietly=TRUE))install.packages("gifski")
+if(!requireNamespace("maps", quietly=TRUE))install.packages("maps")
 if(!requireNamespace("RColorBrewer", quietly=TRUE))install.packages("RColorBrewer")
-if(!requireNamespace("moments", quietly=TRUE))install.packages("moments")
-if(!requireNamespace("scales", quietly=TRUE))install.packages("scales")
+
 
 library(shiny)
 library(dplyr)
 library(tidyverse)
+library(maps)
 library(RColorBrewer)
 library(moments)
 library(scales)
-library(ggplot2)
-library(gganimate)
-
 
 source("./cost_of_living.R")
 source("./shifts_income.R")
 dff <- read.csv("./combined_new.csv", header = TRUE,sep = ',')
 
-source("./preprocess.R")
-source("./gender_imbalance_plot.R")
-source("./living_cost_migration_plot.R")
-IDB <- load_IDB()
-DB_MAP <- load_WorldMap()
-colname2name_map <- attr(IDB, "colname2name_map")
 
 function(input, output) {
   
-  output$gender_imbalance <- gender_imbalance_plot(input, DB_MAP, IDB)
   
-  output$living_cost_migration <- living_cost_migration_plot(input, IDB, output)
   
   output$tab5_plot <- cost_of_living(input, dff, output)
   output$tab6_plot <- shifts_income(input, dff, output)
-  
-  ###< MAP ###
-  worldMapIDB <- reactive({
-    filteredYears <- filter(IDB, Year == input$mapYear)
-    country_data  <- data.frame(country = filteredYears["Country"], occurrences = filteredYears[input$d4])
-  })
-
-  worldMapIntervals <- reactive({
-    d4 <- input$d4
     
-    d4 = gsub(r"{\s*\([^\)]+\)}","",as.character(d4))
-    
-    if(d4 == "Population.Density."){
-      d4 <- "Population.Density..People.per.Sq..Km.."
-    }
-    
-    if(d4 == "Annual.Growth.Rate.%"){
-      d4 <- "Annual.Growth.Rate.."
-    }
-    
-    filteredInput <- worldMapIDB()[d4][!is.na(worldMapIDB()[d4])]
-    
-    minimumPop = min(filteredInput)
-    maximumPop = max(filteredInput)
-    
-    lowestNumber = 0
-    
-    if (minimumPop < lowestNumber){
-      lowestNumber = minimumPop
-    }else {
-      lowestNumber = 0
-    }
-    
-    breakNumber = 7
-    
-    if (maximumPop > 1000){
-      breakNumber = 12
-    }else if (maximumPop < 10){
-      breakNumber = 4
-    }else{
-      breakNumber = 6
-    }
-    
-    
-    if (d4 == "Population.Density..People.per.Sq..Km.."){
-      breakNumber = 7
-      maximumPop = 600
-    }
-    
-    intervals <- cbreaks(c(lowestNumber, maximumPop), breaks_pretty(breakNumber), labels = comma_format())
-    
-  })
-  
-  worldMapDataset <- reactive({
-    DB_MAP <- left_join(DB_MAP, worldMapIDB(), by = c("region" = "Country"))
-    if (d4 == "Net.international.migrants.both.sexes"){
-      breakNumber = 4
-      maximumPop = 1000
-    }
-    
-    intervals <- cbreaks(c(lowestNumber, maximumPop), breaks_pretty(breakNumber), labels = comma_format())
-    
-  })
+   
+  }
   
   
-  ### OVERALL MAP --->###
-  output$map <- renderPlot({
-    
-    d4 <- input$d4
-    
-    #Cleaning input due to change of column names when joining
-    d4 = gsub(r"{\s*\([^\)]+\)}","",as.character(d4))
-    
-    if(d4 == "Population.Density."){
-      d4 <- "Population.Density..People.per.Sq..Km.."
-    }
-    
-    if(d4 == "Annual.Growth.Rate.%"){
-      d4 <- "Annual.Growth.Rate.."
-    }
-    
-    
-    #Loading the proper intervals for the data and creating labels for them
-    color_intervals2 <- worldMapIntervals()
-    
-    colorLabels = head(color_intervals2$labels, -1)
-    colorLabels[1] = paste("Up to", colorLabels[2])
-    
-    for (i in 2:length(colorLabels)){
-      
-      if(!is.na(colorLabels[i+1])){
-        colorLabels[i] = paste(colorLabels[i], "to", colorLabels[i+1])
-      }else{
-        colorLabels[i] = paste(colorLabels[i], "and", "more")
-      }
-
-    }
-    
-    #Clean input for title and map title
-    title <- gsub("\\.(?=[^.]*\\.)", " ", d4, perl=TRUE)
-    mapTitle  = paste("World Map for", title)
-    
-    color_palette <- brewer.pal(length(color_intervals2$breaks), "YlOrRd")
-    
-    filteredWorldMap <- worldMapDataset() %>% select(long,lat, region)
-    
-    #First summarize the region's mean to get the location of the country names to be put on the map
-    cnames  <- filteredWorldMap %>% group_by(region) %>% summarise_all(mean)
-    filterList <- c("China", "Russia", "Brazil", "Australia", "India")
-    cnames <- filter(cnames, region %in% filterList)
-    
-    world_map <- ggplot(worldMapDataset()) +
-      geom_polygon(aes(x = long, y = lat, group = group, fill = cut(.data[[d4]], breaks = color_intervals2$breaks, labels = colorLabels)), color = "gray40") +
-      expand_limits(x = worldMapDataset()$long, y = worldMapDataset()$lat) +
-      geom_text(data = cnames,
-                aes(label = region, x = long, y = lat), size = 3, fontface = 2, color = "gray20") +
-      coord_fixed() +
-      scale_fill_manual(values = color_palette) +
-      labs(title = mapTitle, fill = title) +
-      theme_minimal() +
-      theme(legend.position = "bottom") +
-      theme(legend.position = "bottom",
-            axis.title = element_blank(),
-            axis.text = element_blank(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank())
-    
-    print(world_map)
-  })
-  ###<--- OVERALL MAP###
-  
-  filteredYearFertiltiy <- reactive({
-    filteredYears <- filter(IDB, Year == input$mapYearFertility)
-    country_data  <- data.frame(country = filteredYears[["Country"]], occurrences = filteredYears[["Total.Fertility.Rate"]])
-  })
-  
-  ### FERTILITY MAP --->###
-  output$fertility_map <- renderPlot({
-    
-    filteredInput <- filteredYearFertiltiy()$occurrences[!is.na(filteredYearFertiltiy()$occurrences)]
-    print(filteredInput)
-    
-    minimumPop = min(filteredInput)
-    maximumPop = max(filteredInput)
-    
-    lowestNumber = 0
-    
-    if (minimumPop < lowestNumber){
-      lowestNumber = minimumPop
-    }else {
-      lowestNumber = 0
-    }
-    
-    color_intervals2 <- cbreaks(c(lowestNumber, maximumPop), breaks_pretty(4), labels = comma_format())
-    
-    colorLabels = head(color_intervals2$labels, -1)
-    colorLabels[1] = paste("Up to", colorLabels[2])
-    
-    for (i in 2:length(colorLabels)){
-      
-      if(!is.na(colorLabels[i+1])){
-        colorLabels[i] = paste(colorLabels[i], "to", colorLabels[i+1])
-      }else{
-        colorLabels[i] = paste(colorLabels[i], "and", "more")
-      }
-      
-    }
-    
-    mergedData <- left_join(DB_MAP, filteredYearFertiltiy(), by = c("region" = "country"))
-    
-    color_palette <- brewer.pal(length(color_intervals2$breaks), "Greens")
-    
-    world_map <- ggplot(mergedData) +
-      geom_polygon(aes(x = long, y = lat, group = group, fill = cut(occurrences, breaks = color_intervals2$breaks, labels = colorLabels)), color = "gray40") +
-      coord_fixed() +
-      scale_fill_manual(values = color_palette) +
-      labs(title = "World Map of Fertility Rate", fill = "Births per woman") +
-      theme_minimal() +
-      theme(legend.position = "bottom") +
-      theme(legend.position = "bottom",
-            axis.title = element_blank(),
-            axis.text = element_blank(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank())
-    
-    print(world_map)
-  })
-  ###<--- FERTILITY MAP###
-  
-  ### INFANT MORTALITY MAP --->###
-  filteredYearInfant <- reactive({
-    filteredYears <- filter(IDB, Year == input$mapYearInfant)
-    country_data  <- data.frame(country = filteredYears[["Country"]], occurrences = filteredYears[["Infant.Mortality.Rate.Both.Sexes"]])
-  })
-  
-  output$infant_map <- renderPlot({
-    
-    filteredInput <- filteredYearInfant()$occurrences[!is.na(filteredYearInfant()$occurrences)]
-    
-    minimumPop = min(filteredInput)
-    maximumPop = max(filteredInput)
-    
-    lowestNumber = 0
-    
-    if (minimumPop < lowestNumber){
-      lowestNumber = minimumPop
-    }else {
-      lowestNumber = 0
-    }
-    
-    color_intervals2 <- cbreaks(c(lowestNumber, maximumPop), breaks_pretty(5), labels = comma_format())
-    
-    colorLabels = head(color_intervals2$labels, -1)
-    colorLabels[1] = paste("Up to", colorLabels[2])
-    
-    for (i in 2:length(colorLabels)){
-      if(!is.na(colorLabels[i+1])){
-        colorLabels[i] = paste(colorLabels[i], "to", colorLabels[i+1])
-      }else{
-        colorLabels[i] = paste(colorLabels[i], "and", "more")
-      }
-    }
-    
-    mergedData <- left_join(DB_MAP, filteredYearInfant(), by = c("region" = "country"))
-    
-    color_palette <- brewer.pal(length(color_intervals2$breaks), "YlOrRd")
-    
-    world_map <- ggplot(mergedData) +
-      geom_polygon(aes(x = long, y = lat, group = group, fill = cut(occurrences, breaks = color_intervals2$breaks, labels = colorLabels)), color = "gray40") +
-      coord_fixed() +
-      scale_fill_manual(values = color_palette) +
-      labs(title = "World Map of Infant Mortality Rate, Both Sexes", fill = "Deaths per 1,000 live births") +
-      theme_minimal() +
-      theme(legend.position = "bottom") +
-      theme(legend.position = "bottom",
-            axis.title = element_blank(),
-            axis.text = element_blank(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank())
-    
-    print(world_map)
-  })
-  ### <--- INFANT MORTALITY MAP###
-  
-}
+ 
--- ui.R
+++ ui.R
@@ -1,100 +1,16 @@
 library(shiny)
 library(ggplot2)
 library(readr)
+library(leaflet.extras)
 source("./preprocess.R")
 
 IDB <- load_IDB()
 names_map <- attr(IDB, "name2colname_map")[-c(1:4)]
 
-fluidPage(titlePanel("DV 16"),
-          tabsetPanel(
-            tabPanel(
-              "Fertility Rate",
-              fluid = T,
-              sidebarLayout(
-                position = "right",
-                sidebarPanel(
-                  sliderInput(
-                    "mapYearFertility",
-                    "Map Year:",
-                    min = 2010,
-                    max = 2023,
-                    value = 2011,
-                    step = 2,
-                    sep = "",
-                    animate = TRUE
-                  ),
-                ),
-                mainPanel(plotOutput(outputId = "fertility_map"))
-              )
-            ),
-            tabPanel(
-              "Infant Mortality Rate",
-              fluid = T,
-              sidebarLayout(
-                position = "right",
-                sidebarPanel(
-                  sliderInput(
-                    "mapYearInfant",
-                    "Map Year:",
-                    min = 2010,
-                    max = 2023,
-                    value = 2010,
-                    step = 1,
-                    sep = "",
-                    animate = TRUE
-                  ),
-                ),
-                mainPanel(plotOutput(outputId = "infant_map"))
-              )
-            ),
-            tabPanel(
-              "Gender Imbalance",
-              fluid = T,
-              plotOutput(outputId = "gender_imbalance"),
-              div(
-                style = "display:flex; flex-direction: column; align-items: center; justify-content: center;",
-                sliderInput(
-                  "year_gender_imbalance",
-                  label = div(style = "text-align: center;", "Year:"),
-                  width = "80%",
-                  min = MIN_YEAR,
-                  max = MAX_YEAR,
-                  value = MIN_YEAR,
-                  step = 1,
-                  animate = TRUE
-                ),
-                checkboxInput(
-                  "show_income_level_gender_imbalance",
-                  "Show Income level of the country",
-                  value = F
-                ),
-              ),
-            ),
-            tabPanel(
-              "Living cost vs Migration rate",
-              fluid = T,
-              sidebarLayout(
-                position = "left",
-                sidebarPanel(# selectInput("mode",
-                  #             label="Mode",
-                  #             c("By Year", "By Country")),
-                  # sliderInput(
-                  #   "year_migration_plot",
-                  #   label = "Year:",
-                  #   min = MIN_YEAR,
-                  #   max = MAX_YEAR,
-                  #   value = MIN_YEAR,
-                  #   step = 1,
-                  #   animate = TRUE
-                  # ),
-                  selectInput(
-                    'country_migration_plot', "Country", c(unique(IDB$Country))
-                  ),),
-                mainPanel(imageOutput(outputId = "living_cost_migration"),),
-              )
-            ),
-                tabPanel(
+fluidPage(
+  titlePanel("DV 16"),
+  tabsetPanel(
+    tabPanel(
       "Tab 1", fluid = T,
       sidebarLayout(position = "right",
         sidebarPanel(
@@ -161,22 +77,6 @@ fluidPage(titlePanel("DV 16"),
         )
       )
     )
-            # tabPanel(
-            #   "Map Chart",
-            #   fluid = T,
-            #   sidebarLayout(position = "right",
-            #                 sidebarPanel(
-            #                   selectInput('d4', 'Choose a Variable:', names_map, names_map[[2]]),
-            #                   sliderInput(
-            #                     "mapYear",
-            #                     "Map Year:",
-            #                     min = 2010,
-            #                     max = 2023,
-            #                     value = 2010,
-            #                     step = 1,
-            #                     sep = "",
-            #                     animate = TRUE
-            #                   ),
-            #                 ), )
-            # ),
-          ),)
+    
+  )
+)
